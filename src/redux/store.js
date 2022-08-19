752f86edf6628fb572efdcb16063b97e55d5b2cf
YurijKovbasyuk <93015127+YurijKovbasyuk@users.noreply.github.com>
2022-08-19 21:54:12 +0300

    add local storage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,45 @@
 import { configureStore } from '@reduxjs/toolkit';
 import filter from './filter';
 import contacts from './contacts';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const contactsPersistConfig = {
+  key: 'contacts',
+  version: 1,
+  storage,
+};
+const persistedContactsReducer = persistReducer(
+  contactsPersistConfig,
+  contacts
+);
+const filterPersistConfig = {
+  key: 'filter',
+  version: 1,
+  storage,
+};
+const persistedFilterReducer = persistReducer(filterPersistConfig, filter);
 
 export const store = configureStore({
   reducer: {
-    filter,
-    contacts,
+    filter: persistedFilterReducer,
+    contacts: persistedContactsReducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
